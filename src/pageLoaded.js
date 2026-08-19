b19f389a07cf86009bf700d47aa11f514a399cec
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-19 12:14:45 -0400

    Add: Event Listener

---
+++ src/pageLoaded.js
@@ -0,0 +1,5 @@
+// Injects our content into the content div once page is loaded.
+
+document.addEventListener('DOMContentLoaded', () => {
+    contentDivision = document.getElementById('content')
+})
