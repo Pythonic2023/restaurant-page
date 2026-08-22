1bdc925a2b1b744ccb78a7d916508b0154006e28
Pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-22 15:01:36 -0400

    Add: Solution to removing each child solved.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import { loadMenu } from "./menu.js";
 let header = document.querySelector("header");
 // Listen for events on buttons, call function from module based on button that was clicked.
 header.addEventListener('click', (event) => {
+    let content = document.getElementById('content');
+
     switch(event.target.textContent){
         case "Home":
             console.log("Home clicked");
             break;
         case "Menu":
-            console.log("menu clicked");
+            while(content.hasChildNodes()){
+                content.childNodes.forEach(child => child.remove());
+            }
+            loadMenu();
             break;
         case "About":
             console.log("About clicked");
