8e8117a024c707812d7256408e945fad1d35163f
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-22 13:46:17 -0400

    Add: Styles, setup event listener for buttons.

--- src/pageLoaded.js
+++ src/pageLoaded.js
@@ -1,13 +1,27 @@
 // Injects our content into the content div once page is loaded.
+import burgerfries from "./pictures/burgerfries.jpg";
+import * as styles from "./styles/index.css";
 
 export let onPageLoad = function(){
     document.addEventListener('DOMContentLoaded', () => {
+
         const contentDivision = document.getElementById('content');
+        contentDivision.className = styles["content-division"];
         const header = document.createElement('h1');
         header.textContent = "Sally's Diner, Kickin' good!";
+
+        const burgerPicture = document.createElement('img');
+        burgerPicture.className = styles["burger-picture"];
+        burgerPicture.src = burgerfries;
+        const pictureAttribution = document.createElement("i");
+        pictureAttribution.textContent = "Photo by Mathias Reding";
+
         const introduction = document.createElement('p');
-        introduction.textContent = "Want some home cooked fries, a juicy burger and a shake? Well come to Sally's!"
+        introduction.textContent = "Want some home cooked fries, a juicy burger and a shake? Well, come to Sally's!"
+
         contentDivision.appendChild(header);
+        contentDivision.appendChild(burgerPicture);
+        contentDivision.appendChild(pictureAttribution);
         contentDivision.appendChild(introduction);
     });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import test from "node:test";
+import { resourceLimits } from "node:worker_threads";
+import { type } from "node:os";
 
 export default {
     mode: "development",
@@ -14,4 +17,22 @@ export default {
             template: "./src/template.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader",
+                        options: {modules: true},
+                    }
+                ]
+
+            },
+            {
+                test: /\.(jpg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };
