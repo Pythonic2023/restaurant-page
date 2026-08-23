aa9803f98f2241ec4c2942092fa9ad68604e1389
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-23 09:33:28 -0400

    Add: Tabbed browsing logic, menu page content.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import { onPageLoad } from "./pageLoaded.js";
 import { loadMenu } from "./menu.js";
+import * as styles from "./styles/index.css";
 
 let header = document.querySelector("header");
 // Listen for events on buttons, call function from module based on button that was clicked.
 header.addEventListener('click', (event) => {
     let content = document.getElementById('content');
-
     switch(event.target.textContent){
         case "Home":
             console.log("Home clicked");
             break;
         case "Menu":
-            while(content.hasChildNodes()){
-                content.childNodes.forEach(child => child.remove());
+            let contentExists = (content != null) ? true : false;
+            let menuNull = (document.querySelector("." + styles["menu-division"]) === null) ? true : false;
+            if(contentExists){
+                content.remove();
             }
-            loadMenu();
+            if(menuNull){
+                loadMenu();
+            }
             break;
         case "About":
             console.log("About clicked");
