1dadc5c06f17871bb69f8c4e9c3321921324058a
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-23 09:33:28 -0400

    Add: Tabbed browsing logic, menu page content.

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,28 @@
 import * as styles from "./styles/index.css";
 
 export const loadMenu = function(){
-    const container = document.getElementById("content");
+    const body = document.querySelector("body");
+    const menuContainer = document.createElement("div");
     const menuTitle = document.createElement("h1");
+    menuContainer.className = styles["menu-division"];
     menuTitle.textContent = "Menu";
-    container.appendChild(menuTitle);
+    body.appendChild(menuContainer);
+    menuContainer.appendChild(menuTitle);
+
+    let fooditems = [
+        {menuItem: "Eggs And Bacon", description: "A tasty egger and bacon!"},
+        {menuItem: "Burgers And Fries", description: " A juicy burger and a basket of crispy, homecooked fries."},
+        {menuItem: "The Loaded Poutine", description: "A Big ol' basket of fries with a mean serving of gravy and curds. Freshly cooked bacon will still be sizzlin' when it hits your taste bud's like a gravy train."},
+    ];
+    fooditems.forEach(item => {
+        let foodMenuDiv = document.createElement('div');
+        foodMenuDiv.classList = styles["menu-division"];
+        let menuItem = document.createElement('h2');
+        let menuItemDescription = document.createElement("p");
+        menuItem.textContent = item.menuItem;
+        menuItemDescription.textContent = item.description;
+        menuContainer.appendChild(foodMenuDiv);
+        foodMenuDiv.appendChild(menuItem);
+        foodMenuDiv.appendChild(menuItemDescription);
+    })
 }
