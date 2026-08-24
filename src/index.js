c71c7b4b9f26f8ea07a042dd27a4acd5042a39a9
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-24 12:46:44 -0400

    Add: Content for all page, tab switching buttons.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 import { onPageLoad } from "./pageLoaded.js";
 import { loadMenu } from "./menu.js";
 import * as styles from "./styles/index.css";
+import { loadHome } from "./home.js";
+import { loadContact } from "./contact.js";
 
 let header = document.querySelector("header");
 // Listen for events on buttons, call function from module based on button that was clicked.
 header.addEventListener('click', (event) => {
+    let menu = document.querySelector("." + styles['menu-division']);
     let content = document.getElementById('content');
+    let contact = document.querySelector("." + styles["contact-division"]);
+    let contentExists = (document.getElementById("content") != null) ? true : false;
+    let menuExists = (document.querySelector("." + styles["menu-division"]) != null) ? true : false;
+    let contactExists = (document.querySelector("." + styles["contact-division"]) != null) ? true : false;
     switch(event.target.textContent){
         case "Home":
-            console.log("Home clicked");
-            break;
-        case "Menu":
-            let contentExists = (content != null) ? true : false;
-            let menuNull = (document.querySelector("." + styles["menu-division"]) === null) ? true : false;
+            //let menu = document.querySelector("." + styles["menu-division"]);
+            //let menuExists = (menu != null) ? true : false;
+            if(menuExists){
+                menu.remove();
+            } 
+            if(contactExists){
+                contact.remove();
+            }
             if(contentExists){
                 content.remove();
             }
-            if(menuNull){
+            
+            loadHome();
+            break;
+        case "Menu":
+            //let menuNull = (document.querySelector("." + styles["menu-division"]) === null) ? true : false;
+            if(contentExists){
+                content.remove();
+            } 
+            if(contactExists){
+                contact.remove();
+            }
+            if(!menuExists){
                 loadMenu();
             }
             break;
-        case "About":
-            console.log("About clicked");
+        case "Contact":
+            if(menuExists){
+                menu.remove()
+            }
+            if(contentExists){
+                content.remove();
+            }
+            if(!contactExists){
+                loadContact();
+            }
             break; 
         default:
             console.log("None match");
