326ad4a2a29273c020c53cab6a1d26efdcb90c82
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-08-27 13:00:34 -0400

    Add: common, dev, prod webpack files and merge.

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: './src/template.html',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader",
+                        options: {modules: true},
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
